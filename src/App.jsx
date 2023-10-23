7b96616aaa519352bf690ccb3428dccd6866d0de
Rene Alejo <ralejomorejon@gmail.com>
2023-10-23 21:22:18 +0200

    todo list

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import { NightModeToggle } from "./NightModeToggle"
+import { TodoList } from "./components/TodoList"
 
 function App() {
+ return(
+  <>
 
-  return (
-    <>
-   <NightModeToggle ></NightModeToggle>
- 
+  <div className="card-to-do">
+    <h1>Lista de tareas</h1>
+    <div className="counter-todos">
+      <h3>No. de Tareas: 4</h3>
+      <h3>Pendientes:3</h3>
+    </div>
 
-   </>
-  )
-}
-
+    <div className="add-todo">
+      <h3></h3>
+    </div>
+  </div>
+  <TodoList></TodoList>
+  </>
+ )
+ };
 export default App
